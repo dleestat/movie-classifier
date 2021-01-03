da9ef536adc8ddabf460141395582bf0a580f40a
Daniel Lee <danielleestat@gmail.com>
2021-01-03 13:37:56 -0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,17 @@
+import dash
+from dash.dependencies import Input, Output, State
+import dash_core_components as dcc
+import dash_html_components as html
+import joblib
+import pandas as pd
+
+model = joblib.load('model/model.joblib')
+app = dash.Dash(__name__)
+server = app.server
+app.title = 'Movie Genre Predictor'
+app.layout = html.Div([
+    html.H1('Movie Genre Predictor')
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
