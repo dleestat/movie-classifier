22646cedc03572806f3c4f217eb81774dc7f917e
Daniel Lee <danielleestat@gmail.com>
2021-01-03 14:03:36 -0800

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,11 @@ app = dash.Dash(__name__)
 server = app.server
 app.title = 'Movie Genre Predictor'
 app.layout = html.Div([
-    html.H1('Movie Genre Predictor')
+    html.H1('Movie Genre Predictor'),
+    html.Div([
+        dcc.Textarea(id='input', className='flex-item'),
+        html.Figure(className='flex-item')
+    ], className='flex-container')
 ])
 
 if __name__ == '__main__':
