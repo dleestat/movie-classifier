6e351aeba7d47a5f01b4c0bba6bcf47d59290c6f
Daniel Lee <danielleestat@gmail.com>
2021-01-03 11:36:17 -0800

    Create train.py

---
+++ train.py
@@ -0,0 +1,56 @@
+import joblib
+import numpy as np
+import pandas as pd
+from sklearn.compose import make_column_transformer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, f1_score, hamming_loss, jaccard_score, precision_score, recall_score
+from sklearn.model_selection import train_test_split
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import MultiLabelBinarizer
+
+
+genres = pd.read_csv(
+    'data/movie.metadata.tsv',
+    converters={'genres': lambda x: list(eval(x).values())},
+    delimiter='\t',
+    header=None,
+    index_col='id',
+    names=['id', 'genres'],
+    usecols=[0, 8])
+
+summaries = pd.read_csv(
+    'data/plot_summaries.txt',
+    delimiter='\t',
+    header=None,
+    index_col='id',
+    names=['id', 'summary'])
+
+df = summaries.merge(genres, on='id').reset_index(drop=True)
+mlb = MultiLabelBinarizer()
+labels = pd.DataFrame(mlb.fit_transform(df.genres), columns=mlb.classes_)
+X, Y = df[['summary']], labels[labels.sum().nlargest(10).sort_index().index]
+X_train, X_val, Y_train, Y_val = train_test_split(X, Y, test_size=10000, random_state=0)
+
+def evaluate(model, X, Y_true):
+    Y_pred = model.predict(X)
+    return pd.Series({
+        '# Samples': len(X),
+        'Subset accuracy': accuracy_score(Y_true, Y_pred),
+        'Accuracy': jaccard_score(Y_true, Y_pred, average='samples', zero_division=1),
+        'Hamming similarity': 1 - hamming_loss(Y_true, Y_pred),
+        'Precision': precision_score(Y_true, Y_pred, average='samples', zero_division=1),
+        'Recall': recall_score(Y_true, Y_pred, average='samples', zero_division=1),
+        'F1': f1_score(Y_true, Y_pred, average='samples', zero_division=1),
+    }, dtype=object)
+
+model = make_pipeline(
+    make_column_transformer((TfidfVectorizer(), 'summary')),
+    MultiOutputClassifier(LogisticRegression(solver='liblinear', random_state=0), n_jobs=-1)
+).fit(X_train, Y_train)
+joblib.dump(model, 'model/model.joblib')
+
+metrics = pd.DataFrame({'Train': evaluate(model, X_train, Y_train), 'Val': evaluate(model, X_val, Y_val)})
+metrics.to_json('model/metrics.json')
+print(metrics)
