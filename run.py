9056bddd6e94d7a7832001f1e71d3b8e1288628d
Daniel Lee <danielleestat@gmail.com>
2021-03-18 12:36:48 -0700

    Update run.py

--- run.py
+++ run.py
@@ -1,8 +1,13 @@
+from src import data
 import sys
 
 
 def main(targets):
-    ...
+    if "data" in targets:
+        data.main()
+
+    if "train" in targets:
+        ...
 
 
 if __name__ == "main":
