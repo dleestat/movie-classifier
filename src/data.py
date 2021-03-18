1e12acb109afadca3f977b6b39e2a5b099f750ed
Daniel Lee <danielleestat@gmail.com>
2021-03-18 12:53:25 -0700

    Update data.py

--- src/data.py
+++ src/data.py
@@ -1,27 +1,32 @@
+import os
 import pandas as pd
 
 
 def main():
     print("===== data =====")
 
+    os.makedirs("data/out", exist_ok=True)
+    os.makedirs("data/profiling", exist_ok=True)
+
     genres = pd.read_csv(
         "data/raw/movie.metadata.tsv",
-        converters={'genres': lambda x: list(eval(x).values())},
-        delimiter='\t',
+        converters={"genres": lambda x: list(eval(x).values())},
+        delimiter="\t",
         header=None,
-        index_col='id',
-        names=['id', 'genres'],
+        index_col="id",
+        names=["id", "genres"],
         usecols=[0, 8])
 
     summaries = pd.read_csv(
         "data/raw/plot_summaries.txt",
-        delimiter='\t',
+        delimiter="\t",
         header=None,
-        index_col='id',
-        names=['id', 'summary'])
+        index_col="id",
+        names=["id", "summary"])
 
-    df = summaries.merge(genres, on='id').reset_index(drop=True)
+    df = summaries.merge(genres, on="id").reset_index(drop=True)
+    df.to_pickle("data/out/data.pkl")
 
 
-if __name__ == "main":
+if __name__ == "__main__":
     main()
