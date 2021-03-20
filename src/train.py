ee03863b83bfb30036f2cf714172898a800baa9e
Daniel Lee <danielleestat@gmail.com>
2021-03-19 17:59:51 -0700

    Update train.py

--- src/train.py
+++ src/train.py
@@ -1,3 +1,4 @@
+import datetime
 import joblib
 import json
 import pandas as pd
@@ -14,33 +15,32 @@ def main():
     def evaluate(model, X, Y_true):
         Y_pred = model.predict(X)
         return {
-            "# Samples": len(X),
-            "Subset accuracy": accuracy_score(Y_true, Y_pred),
-            "Accuracy": jaccard_score(Y_true, Y_pred, average="samples", zero_division=1),
+            "# samples": len(X),
+            "subset accuracy": accuracy_score(Y_true, Y_pred),
+            "accuracy": jaccard_score(Y_true, Y_pred, average="samples", zero_division=1),
             "Hamming similarity": 1 - hamming_loss(Y_true, Y_pred),
-            "Precision": precision_score(Y_true, Y_pred, average="samples", zero_division=1),
-            "Recall": recall_score(Y_true, Y_pred, average="samples", zero_division=1),
-            "F1": f1_score(Y_true, Y_pred, average="samples", zero_division=1)
+            "precision": precision_score(Y_true, Y_pred, average="samples", zero_division=1),
+            "recall": recall_score(Y_true, Y_pred, average="samples", zero_division=1),
+            "f1": f1_score(Y_true, Y_pred, average="samples", zero_division=1)
         }
-
 
     df = pd.read_pickle("data/out/df.pkl")
     X, Y = df[["summary"]], df.drop("summary", axis=1)
     X_train, X_val, Y_train, Y_val = train_test_split(X, Y, test_size=10000, random_state=0)
 
     model = make_pipeline(
-        make_column_transformer((TfidfVectorizer(min_df=10), 0)),
+        make_column_transformer((TfidfVectorizer(), 0)),
         MultiOutputClassifier(LogisticRegression(solver="liblinear", random_state=0), n_jobs=-1)
     )
 
     model.fit(X_train, Y_train)
-    metrics = {"Train": evaluate(model, X_train, Y_train), "Val": evaluate(model, X_val, Y_val)}
+    metrics = {"train": evaluate(model, X_train, Y_train), "val": evaluate(model, X_val, Y_val)}
     model.fit(X, Y)
-    metrics["Final train"] = evaluate(model, X, Y)
+    metrics["final train"] = evaluate(model, X, Y)
 
-    joblib.dump(model, "../model/model.joblib")
-    with open("../model/metrics.json", "w") as f:
-        json.dump(metrics, f)
+    joblib.dump(model, "model/model.joblib")
+    with open(f"model/{datetime.datetime.today()}.json", "w") as f:
+        json.dump(metrics, f, indent=2)
 
 
 if __name__ == "__main__":
